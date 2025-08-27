d38ddc2950a88c33a191cb0de35497e98c3610f2
Angela Monsalve <angelapmonsalve@gmail.com>
2025-08-27 16:43:06 -0500

    feat: agregar AdaptersConfig y limpiar configs/tests obsoletos

---
+++ applications/app-service/src/main/java/com/angelapmonsalve/microservices/autenticacion/config/AdaptersConfig.java
@@ -0,0 +1,24 @@
+package com.angelapmonsalve.microservices.autenticacion.config;
+
+import com.angelapmonsalve.microservices.autenticacion.model.usuario.gateways.LoggerService;
+import com.angelapmonsalve.microservices.autenticacion.model.usuario.gateways.UsuarioRepository;
+import com.angelapmonsalve.microservices.autenticacion.r2dbc.adapter.UsuarioRepositoryAdapter;
+import com.angelapmonsalve.microservices.autenticacion.r2dbc.repository.UsuarioDataRepository;
+import com.angelapmonsalve.microservices.autenticacion.logger.Slf4jLoggerAdapter;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdaptersConfig {
+
+    @Bean
+    public UsuarioRepository usuarioRepository(UsuarioDataRepository repository, ObjectMapper mapper) {
+        return new UsuarioRepositoryAdapter(repository, mapper);
+    }
+
+    @Bean
+    public LoggerService loggerService() {
+        return new Slf4jLoggerAdapter();
+    }
+}
