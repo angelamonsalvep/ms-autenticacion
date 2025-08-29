86137f1fd1a95a133392dbdab419987b846220c1
Angela Monsalve <angelapmonsalve@gmail.com>
2025-08-28 22:22:44 -0500

    refactor(persistence): ajustes en entidad UsuarioData para modificar el tipo del campo id

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/com/angelapmonsalve/microservices/autenticacion/r2dbc/entities/UsuarioData.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/com/angelapmonsalve/microservices/autenticacion/r2dbc/entities/UsuarioData.java
@@ -5,9 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -17,13 +19,21 @@ import java.time.LocalDate;
 public class UsuarioData {
 
     @Id
-    private String id;
+    private UUID id;   // corresponde al tipo uuid en Postgres
 
     private String nombres;
     private String apellidos;
+
+    @Column("fecha_nacimiento")
     private LocalDate fechaNacimiento;
+
     private String direccion;
     private String telefono;
+
+    @Column("correo_electronico")
     private String correoElectronico;
+
+    @Column("salario_base")
     private Double salarioBase;
+
 }
