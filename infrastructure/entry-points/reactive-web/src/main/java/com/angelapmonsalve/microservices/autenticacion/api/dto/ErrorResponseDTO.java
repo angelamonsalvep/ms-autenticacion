9ad02922e1da062622c4d4747069a0a73cfb997c
Angela Monsalve <angelapmonsalve@gmail.com>
2025-08-28 22:01:53 -0500

    feat(api): crear ErrorResponseDTO para estandarizar respuestas de error

--- infrastructure/entry-points/reactive-web/src/main/java/com/angelapmonsalve/microservices/autenticacion/api/dto/ErrorResponseDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/angelapmonsalve/microservices/autenticacion/api/dto/ErrorResponseDTO.java
@@ -1,4 +1,12 @@
 package com.angelapmonsalve.microservices.autenticacion.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorResponseDTO {
+    private String codigo;
+    private String mensaje;
 }
+
