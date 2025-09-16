846e199587de5772e750b6928fbf0cad70f20b10
Angela Monsalve <angelapmonsalve@gmail.com>
2025-09-16 00:00:15 -0500

    fix(jwt): ajustes en obtención de rol desde el token y manejo de excepciones

--- infrastructure/driven-adapters/jwt/src/main/java/com/angelapmonsalve/microservices/autenticacion/jwt/JwtTokenProvider.java
+++ infrastructure/driven-adapters/jwt/src/main/java/com/angelapmonsalve/microservices/autenticacion/jwt/JwtTokenProvider.java
@@ -2,6 +2,8 @@ package com.angelapmonsalve.microservices.autenticacion.jwt;
 
 import com.angelapmonsalve.microservices.autenticacion.model.login.Login;
 import com.angelapmonsalve.microservices.autenticacion.model.login.gateways.TokenProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -23,5 +25,18 @@ public class JwtTokenProvider implements TokenProvider {
             .signWith(SECRET_KEY)
             .compact();
         return Mono.just(token);
+    }
+
+    public String getRolFromToken(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token.replace("Bearer ", ""))
+                .getBody();
+            return claims.get("rol", String.class);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RuntimeException("Token inválido o expirado");
+        }
     }
 }
